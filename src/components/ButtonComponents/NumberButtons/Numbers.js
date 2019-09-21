a12faf9b1cb7739b6ae053d3b193d169b6ca3f40
Jake <panamajake86@gmail.com>
2019-09-20 22:36:49 -0600

    After having my TL hold my hand and walk me through this I finally got all the buttons to appear.  I'm not sure what I'm going to do about styling these things but that is a project for later.

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,20 +1,24 @@
 import React from "react";
-import numbers from "../../../data.js";
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+
+import { numbersData } from '../../../data' 
+import {NumberButton} from "./NumberButton"
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
-  const [number, setNumber] = useState(numbers);
+  const [number, setNumber] = React.useState(numbersData);
+
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {number.map((num, i)=>{
+        return <NumberButton number={num} key={i} />
+      })}
+
     </div>
   );
 };
